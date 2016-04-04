d6feef2609889fc3d0cc61cc143a16d7fabae439
Sammi Wu Leung <swuleung@stuy.edu>
2016-04-04 02:17:45 +0000

    update

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 import database, os
+import explore
 app = Flask(__name__)
+
+def getSources(): #Putting all Outside Sources into list 'srcs'
+    srcs = []
+    f = open(os.path.join(os.path.dirname(__file__), "misc/sources.txt"), "r")
+    for line in f:
+        line = line[0:-1] #-1 is to remove the newline character
+        line = line.replace("https","")
+        line = line.replace("http","")
+        line = line.replace("//","")
+        line = line.replace(":","")
+        line = line.replace("www.","")
+        srcs.append(line)
+    return srcs
+srcs = getSources()
 
 @app.route("/")
 @app.route("/home")
 def home(): 
     if ('id' not in session):
         session['id'] = -1
-    session['id'] = -1
+    #session['id'] = -1
     if session['id'] == -1:
         return render_template("home.html")
     else:
@@ -37,9 +52,8 @@ def login():
         password = request.form['password']
         verifylogin = database.valid_login(username, password)
         if verifylogin != -1:
-            session['type'] = 'parent'
             session['id'] = verifylogin
-            return redirect("home")
+            return redirect("study")
         else:
             return render_template("login.html", error=True)
     else:
@@ -50,10 +64,19 @@ def search():
     if session['id'] == -1:
         return redirect(url_for("home"))
     if request.method == 'POST':
-        return render_template("results.html")
+        q = request.form["searchTerm"]
+        return explore.googleSearch(q)
     else:
         return render_template("search.html")
-        
+
+@app.route("/study")
+def study():
+    if session['id'] == -1:
+        return redirect(url_for("home"))
+    #add results
+    return render_template("study.html")
+    
+    
 @app.route("/logout")
 def logout():
     #resets the session to none
